6b8c568c469027468d0d3e02540a8d5e9af9d36c
Tobias Thiele <thiele231@web.de>
2023-10-31 06:27:30 +0100

    hotfix

--- admin/src/pages/HomePage/index.js
+++ admin/src/pages/HomePage/index.js
@@ -3,32 +3,30 @@
  * HomePage
  *
  */
-import './index.css';
-import {dracula} from '@uiw/codemirror-theme-dracula';
+import "./index.css";
+import { dracula } from "@uiw/codemirror-theme-dracula";
 
+import React, { memo, useEffect, useState } from "react";
+import { ContentLayout, HeaderLayout } from "@strapi/design-system/Layout";
+import { request, useNotification } from "@strapi/helper-plugin";
+import { Badge, Box, Button, Flex, Table, Tbody, Td, Th, Thead, Tr } from "@strapi/design-system";
 
-import React, {memo, useEffect, useState} from 'react';
-import {ContentLayout, HeaderLayout} from '@strapi/design-system/Layout';
-import {request, useNotification} from '@strapi/helper-plugin';
-import { Button, Box, Table, Thead, Tbody, Tr, Th, Td, Badge, Flex} from '@strapi/design-system';
+import CodeMirror from "@uiw/react-codemirror";
+import { langs } from "@uiw/codemirror-extensions-langs";
+import getTrad from "../../utils/getTrad";
+import pluginId from "../../pluginId";
 
-import CodeMirror from '@uiw/react-codemirror';
-import {langs} from '@uiw/codemirror-extensions-langs';
-import getTrad from '../../utils/getTrad';
-import pluginId from '../../pluginId';
-
-import * as pkg from '../../../../package.json';
+import * as pkg from "../../../../package.json";
 
 function HomePage() {
-
   const toggleNotification = useNotification();
 
-  const [code, setCode] = useState(
-    'SELECT * FROM admin_users LIMIT 1;'
+  const [ code, setCode ] = useState(
+    "SELECT * FROM admin_users LIMIT 1;",
   );
 
-  const [tableData, setTableData] = useState(null);
-  const [executing, setExecuting] = useState(false);
+  const [ tableData, setTableData ] = useState(null);
+  const [ executing, setExecuting ] = useState(false);
 
   const editorDidMount = (editor, monaco) => {
     const code = window.localStorage.getItem(`${pluginId}_code`);
@@ -38,12 +36,12 @@ function HomePage() {
       editor.setValue(code);
     }
     editor.focus();
-  }
+  };
 
   const onChange = (value) => {
     window.localStorage.setItem(`${pluginId}_code`, value);
     setCode(value);
-  }
+  };
 
   useEffect(() => {
     const tmpCode = window.localStorage.getItem(`${pluginId}_code`);
@@ -59,16 +57,16 @@ function HomePage() {
       setTableData(null);
 
       const response = await request(`/${pluginId}/execute`, {
-        method: 'POST',
+        method: "POST",
         body: {
-          code: code.replace(/(\r\n|\n|\r)/g, ' '),
+          code: code.replace(/(\r\n|\n|\r)/g, " "),
         },
       });
 
       toggleNotification({
-        type: 'success',
+        type: "success",
         message: {
-          id: `${getTrad('notification.info.execute.success')}`,
+          id: `${getTrad("notification.info.execute.success")}`,
         },
       });
       setTableData(response.response);
@@ -76,29 +74,30 @@ function HomePage() {
     } catch (err) {
       console.error(err);
       toggleNotification({
-        type: 'warning',
+        type: "warning",
         message: {
-          id: `${getTrad('notification.info.execute.error')}`,
+          id: `${getTrad("notification.info.execute.error")}`,
         },
       });
     } finally {
       setExecuting(false);
     }
-  }
+  };
 
   const getTableHeaders = (data) => {
-    let headers = [];
+    console.log(data);
+    const headers = [];
     for (const dataKey in data) {
       headers.push(dataKey);
     }
 
     return headers;
-  }
+  };
 
   const getTableRows = (data) => {
-    let rows = [];
-    data.forEach(d => {
-      let r = [];
+    const rows = [];
+    data.forEach((d) => {
+      const r = [];
       for (const dk in d) {
         r.push(d[dk]);
       }
@@ -106,16 +105,16 @@ function HomePage() {
     });
 
     return rows;
-  }
+  };
 
   const handleDownloadCSV = () => {
     let headersCsv = [];
     let rowsCsv = [];
 
-    if (tableData.result.length) {
-      const tableHeaders = getTableHeaders(tableData.result[0]);
+    if (tableData.result[0]?.length) {
+      const tableHeaders = getTableHeaders(tableData.result[0][0]);
       headersCsv = headersCsv.concat(tableHeaders);
-      const tableRows = getTableRows(tableData.result);
+      const tableRows = getTableRows(tableData.result[0]);
       rowsCsv = rowsCsv.concat(tableRows);
     }
 
@@ -124,20 +123,20 @@ function HomePage() {
     const fileName = `export_query_strapi_sql_${formattedDate}.csv`;
 
     // Generate CSV content
-    const csvHeader = headersCsv.join(',');
-    const csvRows = rowsCsv.map(row => row.join(',')).join('\n');
+    const csvHeader = headersCsv.join(",");
+    const csvRows = rowsCsv.map(row => row.join(",")).join("\n");
     const csvContent = `${csvHeader}\n${csvRows}`;
 
     // Create a Blob object to keep special characters
-    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const blob = new Blob([ csvContent ], { type: "text/csv;charset=utf-8;" });
 
     // Create a URL for the Blob
     const url = window.URL.createObjectURL(blob);
 
     // Create a link element for downloading
-    const link = document.createElement('a');
-    link.setAttribute('href', url);
-    link.setAttribute('download', fileName);
+    const link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("download", fileName);
 
     // Add the link to the document and trigger the download
     document.body.appendChild(link);
@@ -157,14 +156,14 @@ function HomePage() {
       />
       <ContentLayout>
         <CodeMirror
-          extensions={[langs.sql()]}
+          extensions={[ langs.sql() ]}
           theme={dracula}
           height="200px"
           value={code}
           options={{
-            mode: 'sql',
-            theme: 'dracula',
-            lineNumbers: true
+            mode: "sql",
+            theme: "dracula",
+            lineNumbers: true,
           }}
           editorDidMount={editorDidMount}
           onChange={onChange}
@@ -179,51 +178,51 @@ function HomePage() {
             Execute
           </Button>
         </div>
-        <div style={{overflow: 'auto', margin: '24px 0px'}}>
-          {tableData &&
-            <div className="raw-query_query">
+        <div style={{ overflow: "auto", margin: "24px 0px" }}>
+          {tableData
+            && <div className="raw-query_query">
               <Flex justifyContent="space-between" alignItems="center" paddingBottom={4}>
-                <Badge>{tableData.result.length} Result{tableData.result.length > 1 ? 's' : ''}</Badge>
+                <Badge>{tableData.result[0].length} Result{tableData.result[0].length > 1 ? "s" : ""}</Badge>
 
                 <Button
                   variant="secondary"
                   onClick={handleDownloadCSV}
-                  disabled={!tableData || !tableData.result.length}
+                  disabled={!tableData || !tableData.result[0]?.length}
                 >
                   Export CSV
                 </Button>
               </Flex>
               <Box>
-                <Table colCount={getTableHeaders(tableData.result).length} rowCount={tableData.result[0]?.length || 0}>
+                <Table>
                   <Thead>
                     <Tr>
                       {
-                        getTableHeaders(tableData.result[0]).map((th, index) => {
+                        getTableHeaders(tableData.result[0][0]).map((th, index) => {
                           return (
-                            <Th style={{padding: '16px', fontWeight: 'bold'}} key={`th_${index}`}>
+                            <Th style={{ padding: "16px", fontWeight: "bold" }} key={`th_${index}`}>
                               {th}
                             </Th>
-                          )
+                          );
                         })
                       }
                     </Tr>
                   </Thead>
                   <Tbody>
                     {
-                      getTableRows(tableData.result).map((tr, index) => {
+                      getTableRows(tableData.result[0]).map((tr, index) => {
                         return (
                           <Tr key={`tr_${index}`}>
                             {
                               tr.map((td, index) => {
                                 return (
-                                  <Td style={{padding: '16px'}} key={`td_${index}`}>
+                                  <Td style={{ padding: "16px" }} key={`td_${index}`}>
                                     {td}
                                   </Td>
-                                )
+                                );
                               })
                             }
                           </Tr>
-                        )
+                        );
                       })
                     }
                   </Tbody>
